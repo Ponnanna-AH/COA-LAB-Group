ecc8758a2c4952e444ba977709312d0af98c462b
Ponnanna-AH <siliconguy878@gmail.com>
2023-11-06 23:49:14 +0530

    Part One Done

--- COA_LAB_Endsem_project/gpgpu-sim_distribution/src/gpgpu-sim/shader.cc
+++ COA_LAB_Endsem_project/gpgpu-sim_distribution/src/gpgpu-sim/shader.cc
@@ -1515,7 +1515,7 @@ void kaws_scheduler::order_warps() {
   if (isKAWS) {
     order_by_priority(m_next_cycle_prioritized_warps, m_supervised_warps,
                     m_last_supervised_issued, m_supervised_warps.size(),
-                    ORDERING_GREEDY_THEN_PRIORITY_FUNC,
+                    ORDERING_BY_CTA_PROGRESS,
                     scheduler_unit::sort_warps_by_cta_progress);
   } else {
     order_lrr(m_next_cycle_prioritized_warps, m_supervised_warps,
